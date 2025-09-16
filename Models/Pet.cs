6cf2813353f0d28f27151d6bfa46f0ca4fc472df
Sbongumusa2003 <u22710362@tuks.co.za>
2025-09-16 06:47:49 +0200

    King

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -14,7 +14,7 @@ namespace u22710362_HW2.Models
 		public int BreedID { get; set; }
 		public string BreedName { get; set; }
 		public string LocationID { get; set; }
-		public int LocationName { get;set; }
+		public string LocationName { get;set; }
 		public int Age { get; set; }
 		public decimal Weight { get; set; }
 		public string Gender { get; set; }
@@ -24,7 +24,7 @@ namespace u22710362_HW2.Models
 		public string PostedByFirstName { get; set; }
 		public string PostedByLastName { get; set; }
 		public int? AdoptedByUserID { get; set; }
-		public string AdoptedByUserFirstName { get; set; }
+		public string AdoptedByFirstName { get; set; }
 		public string AdoptedByLastName { get; set; }
 		public string B64Image { get; set; }
 	}
